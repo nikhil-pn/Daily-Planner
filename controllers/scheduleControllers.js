7f00c69514431559942bcca6517223d358d7473f
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-25 17:41:03 +0530

    create schedule complete web noticaiton implementd

--- controllers/scheduleControllers.js
+++ controllers/scheduleControllers.js
@@ -1,10 +1,25 @@
 const RedisClient = require("../config/connectRedis");
+const webPush = require("web-push");
+
+require("dotenv").config();
+
+const publicKey = process.env.PUBLIC_KEY;
+const privateKey = process.env.PRIVATE_KEY;
+
+webPush.setVapidDetails("mailto:test@test.com", publicKey, privateKey)
 
 const createSchedule = async (req, res) => {
   try {
-    const { schedule } = req.body;
+    const { schedule , subscription} = req.body;
     await RedisClient.set("schedule", JSON.stringify(schedule));
-    const savedSchedule =  await RedisClient.get("schedule");
+    await RedisClient.set("subscription", JSON.stringify(subscription));
+    const savedSchedule = await RedisClient.get("schedule");
+    
+    webPush.sendNotification(subscription, JSON.stringify({
+      title: "Devsnest",
+      body: "First push notfication"
+    }))
+    
     res.status(200).json({ message: savedSchedule });
   } catch (error) {
     console.log(error);
