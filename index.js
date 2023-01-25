543f035e5d63cbc5eb639c39f2571c9313c15b55
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-25 11:53:35 +0530

    reddis connect added to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
-
+const RedisClient = require("./config/connectRedis");
 const PORT = 3001;
 
 app.use(express.json());
@@ -9,4 +9,11 @@ app.use(logger("dev"));
 
 app.listen(PORT, () => {
   console.log("Server is runnig");
+  RedisClient.connect()
+    .then(() => {
+      console.log("Connected to redis");
+    })
+    .catch((e) => {
+      console.log(e, "DB failed to connect");
+    });
 });
